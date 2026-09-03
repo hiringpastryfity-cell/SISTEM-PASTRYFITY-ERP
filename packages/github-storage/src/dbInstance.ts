1fac398cce4b902ad60855023c8bca1971657773
hiringpastryfity-cell <hiringpastryfity@gmail.com>
2026-09-03 08:57:52 +0700

    Update dbInstance.ts

--- packages/github-storage/src/dbInstance.ts
+++ packages/github-storage/src/dbInstance.ts
@@ -3,8 +3,8 @@ import { GithubLightweightDB } from '@pastryfity/github-storage';
 // =====================================================================
 // ⚙️ KONFIGURASI GITHUB API
 // =====================================================================
-const TOKEN_PART_1 = 'github_pat_11CLESV4Q07no0fYIeVsVD_'; 
-const TOKEN_PART_2 = 'S44zLrixc5oVf9j1UI9H4yxpZiC0aYwnI7016I0VilMOHWEHJTIPUm2jsGa'; 
+const TOKEN_PART_1 = 'github_pat_11CNG6K6A0NJorHn7YfOVg_'; 
+const TOKEN_PART_2 = 'BjR5e9e6mS4QBiLBLbckBsPRileloUvB79pgv8NTarVC25UZPW3UbfDyix5'; 
 const GITHUB_TOKEN = TOKEN_PART_1 + TOKEN_PART_2; 
 
 const REPO_OWNER = 'hiringpastryfity-cell';         
